6650995e97dd42a0a5abba5b183181e96f72c6d9
Sagar Sarkar <sarkars1@my.erau.edu>
2024-03-26 08:52:48 -0400

    Changes in ros_bag/Capstone/v.1

--- src/ros_bag/Capstone/v.1/chassis_cutout.m
+++ src/ros_bag/Capstone/v.1/chassis_cutout.m
@@ -51,10 +51,13 @@ for i = 1:length(imd.Files)
 
     imshow(mask);
     hold on
-    viscircles([centers2(valid_ceneter2,:);sq_plt_center],[diameters2(valid_ceneter2)/2;diameters1(valid_ceneter1)/2],"Color","b");
+    viscircles(centers2(valid_ceneter2,:),diameters2(valid_ceneter2)/2,"Color","b");
     plot(centers2(valid_ceneter2,1),centers2(valid_ceneter2,2),"-r","LineWidth",2)
     hold off
 
+    % frame = getframe(figure(1));
+    % file = frame2im(frame);
+    % imwrite(file,"img_"+num2str(i)+".png")
     pause(0.02)
 end
 
@@ -62,9 +65,99 @@ min_dist = min(distBetweenChassisCutouts);
 max_dist = max(distBetweenChassisCutouts);
 final_dist_values = (distBetweenChassisCutouts - min_dist)/max_dist;
 height = [0:2:30 40 50]';
-model = fit(height,final_dist_values,'poly2');
 
-figure(2)
-plot(model,height,final_dist_values)
-grid on
-grid minor
+% figure(1)
+% plot(height,final_dist_values)
+% xlabel("Height")
+% ylabel("Distance between the two chassis cutouts (Normalized)")
+% grid on
+% grid minor
+%% Spline
+clc;
+% Example data points (replace with your actual data)
+x = height;
+y = final_dist_values;
+query_points = (0:0.1:50)';
+% Perform spline interpolation
+spline_curve = spline(x, y, query_points);
+
+% Plot the original data points and the fitted spline curve
+plot(x, y, 'bo');  % Plot data points
+hold on;
+plot(query_points,spline_curve, 'r-');  % Plot fitted spline curve
+xlabel('x');
+ylabel('y');
+title('Fitted Spline Curve');
+legend('Data Points', 'Fitted Spline Curve');
+grid on;
+hold off;
+%% Test
+clc;clear;close all
+load("camera_params.mat")
+load("Data.mat")
+sub = rossubscriber("/tracking","sensor_msgs/Image","DataFormat","struct");
+i = 1;
+figure(1)
+while true
+    chk = [];
+    msg = receive(sub);
+    img = rosReadImage(msg);
+    img = undistortFisheyeImage(img,Params.Intrinsics);
+    T = adaptthresh(img,"Statistic","gaussian");
+    bw = imbinarize(img,T);
+
+    mask = ~bw;
+    CC1 = bwconncomp(mask);
+    stats1 = regionprops("table",CC1,"Centroid", ...
+    "MajorAxisLength","MinorAxisLength","Area");
+    centers1 = stats1.Centroid;
+    chk(:,1) = 310 < centers1(:,1);
+    chk(:,2) = 325 > centers1(:,1);
+    diameters1 = mean([stats1.MajorAxisLength stats1.MinorAxisLength],2);
+    valid_ceneter1 = false(size(centers1,1));
+    for j = 1:size(centers1,1)
+        if all(chk(j,:)) && 50 < stats1.Area(j) && 10000 > stats1.Area(j)
+            valid_ceneter1(j) = true;
+        end
+    end
+    sq_plt_center = centers1(valid_ceneter1,:);
+
+    
+    CC2 = bwconncomp(bw);
+    stats2 = regionprops("table",CC2,"Centroid", ...
+    "MajorAxisLength","MinorAxisLength","Area");
+    centers2 = stats2.Centroid;
+    diameters2 = mean([stats2.MajorAxisLength stats2.MinorAxisLength],2);
+    
+    valid_ceneter2 = false(size(centers2,1));
+    for j = 1:size(centers2,1)
+        angle = atan2((centers2(j,2) - sq_plt_center(1,2)),(centers2(j,1) - sq_plt_center(1,1)));
+        if (2.4 > angle && 2 < angle && 1500 < stats2.Area(j) && 7000 > stats2.Area(j)) ||...
+           (1.5 > angle && 0.7 < angle && 1500 < stats2.Area(j) && 7000 > stats2.Area(j))
+            valid_ceneter2(j) = true;
+            radii2 = diameters2(j)/2;
+            chassis_ceneter = centers2(j,:);
+            
+        end
+    end
+    delX = diff(centers2(valid_ceneter2,1));
+    delY = diff(centers2(valid_ceneter2,2));
+    distBetweenChassisCutouts = sqrt(delX^2 + delY^2);
+    normalized_dist(i) = (distBetweenChassisCutouts - min_dist)/max_dist;
+    height(i) = spline(spline_curve,query_points,normalized_dist(i));
+    fprintf("Current height: %.4f\n",height(i))
+
+    imshow(mask);
+    hold on
+    viscircles(centers2(valid_ceneter2,:),diameters2(valid_ceneter2)/2,"Color","b");
+    plot(centers2(valid_ceneter2,1),centers2(valid_ceneter2,2),"-r","LineWidth",2)
+    hold off
+    i = i+1;
+end
+%%
+plot(height,normalized_dist,'-ok');
+xlabel('Normalized feature values');
+ylabel('Height');
+title('UAV height vs Distance of chassis cutout');
+grid on;
+grid minor;
